c1f0f2c4d73bbb2500930926a48207df1ca4ad30
Monicah Ajeso <96474976+Ajeso@users.noreply.github.com>
2026-02-12 15:26:34 +0300

    Added the if statement to check for opacity and increase it with every iteration

--- main.js
+++ main.js
@@ -8,10 +8,25 @@ for (let i = 1; i < size * size; i++) {
   container.appendChild(div);
 
   div.addEventListener("mouseover", () => {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    let color = `rgb(${r}, ${g}, ${b})`;
-    div.style.backgroundColor = color;
+    if (!div.baseColor) {
+      let r = Math.floor(Math.random() * 256);
+      let g = Math.floor(Math.random() * 256);
+      let b = Math.floor(Math.random() * 256);
+      div.baseColor = { r, g, b };
+      div.darknessLevel = 0;
+    }
+
+    if (div.darknessLevel < 10) {
+      let r = div.baseColor.r;
+      let g = div.baseColor.g;
+      let b = div.baseColor.b;
+
+      let newR = Math.floor(r * (1 - 0.1 * div.darknessLevel));
+      let newG = Math.floor(g * (1 - 0.1 * div.darknessLevel));
+      let newB = Math.floor(b * (1 - 0.1 * div.darknessLevel));
+
+      div.style.backgroundColor = `rgb(${newR}, ${newG}, ${newB})`;
+      div.darknessLevel++;
+    }
   });
 }
