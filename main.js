be09ce70ec11b34cb23dfb7841563c300fbcb542
Monicah Ajeso <96474976+Ajeso@users.noreply.github.com>
2026-02-12 14:27:53 +0300

    Added eventListener and generated random colors

--- main.js
+++ main.js
@@ -6,4 +6,12 @@ for (let i = 1; i < size * size; i++) {
   let div = document.createElement("div");
   div.className = "grid-div";
   container.appendChild(div);
+
+  div.addEventListener("mouseover", () => {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let color = `rgb(${r}, ${g}, ${b})`;
+    div.style.backgroundColor = color;
+  });
 }
